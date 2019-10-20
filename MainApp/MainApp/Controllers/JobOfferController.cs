487a2e38f73333f3c8a3c47f55649e0a4e5771f8
JakubBilski <jakub.michal.bilski@gmail.com>
2019-10-20 18:39:14 +0200

    Added JobOffer model, controller (Index and Details) and view. Added Job Offer link to nav bar.

---
+++ MainApp/MainApp/Controllers/JobOfferController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MainApp.Models;
+
+namespace MainApp.Controllers
+{
+    public class JobOfferController : Controller
+    {
+        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer{Id=1, JobTitle="Backend Developer" },
+            new JobOffer{Id=2, JobTitle="Frontend Developer" },
+            new JobOffer{Id=3, JobTitle="Manager" },
+            new JobOffer{Id=4, JobTitle="Teacher" },
+            new JobOffer{Id=5, JobTitle="Cook" }
+        };
+        public IActionResult Index()
+        {
+            return View("Index", _jobOffers);
+        }
+        public IActionResult Details(int id)
+        {
+            if (id > _jobOffers.Count || id < 1)
+                return new NotFoundResult();
+            var model = _jobOffers[id-1];
+            return View("Details", model);
+        }
+        
+    }
+}
