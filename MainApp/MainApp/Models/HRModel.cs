55d99f84e85291c6be9102f5895e0aebc0684854
JakubBilski <jakub.michal.bilski@gmail.com>
2019-10-24 12:57:22 +0200

    Added HR model #152

---
+++ MainApp/MainApp/Models/HRModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainApp.Models
+{
+    public class HRModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+    }
+}
