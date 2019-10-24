2244d29e8f4afe3c1b824309fc7cb85bcf11cb50
jakub-drak-sbahi <jakubdrak98@gmail.com>
2019-10-24 22:11:36 +0200

    Created Candidate's model, view and controller #46

---
+++ MainApp/MainApp/Controllers/CandidateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MainApp.Models;
+
+namespace MainApp.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private static List<Candidate> _candidates = new List<Candidate>
+        {
+            new Candidate{Id=1, FirstName="John", LastName="Black", PhoneNumber="123456789", EmailAddress="john@black.com" },
+            new Candidate{Id=2, FirstName="Kate", LastName="Green", PhoneNumber="123222789", EmailAddress="kate@green.com" },
+            new Candidate{Id=3, FirstName="Joe", LastName="Blue", PhoneNumber="123111789", EmailAddress="joe@blue.com" },
+        };
+
+        public IActionResult Index()
+        {
+            return View("Index", _candidates);
+        }
+    }
+}
---
+++ MainApp/MainApp/Models/CandidateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainApp.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public List<Application> Applications { get; set; } = new List<Application>();
+    }
+}
