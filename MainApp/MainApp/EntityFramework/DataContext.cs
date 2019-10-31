cb5350bc0258e5af71be2a52b7e23427c5252431
jakub-drak-sbahi <jakubdrak98@gmail.com>
2019-10-31 13:00:09 +0100

    Added connection to Database #160

---
+++ MainApp/MainApp/EntityFramework/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MainApp.Models;
+
+namespace MainApp.EntityFramework
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Application> JobApplications { get; set; }
+        public DbSet<JobOffer> JobOffers { get; set; }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
