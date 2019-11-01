8749b333e8c3acb06e4b77a6713ab453d396f7fc
JakubBilski <jakub.michal.bilski@gmail.com>
2019-11-01 18:52:41 +0100

    Added delete-edit-create functionality to JobOffer
    Related Work Items: #38, #171, #173, #174, #175, #176

--- MainApp/MainApp/Controllers/JobOfferController.cs
+++ MainApp/MainApp/Controllers/JobOfferController.cs
@@ -5,32 +5,121 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MainApp.Models;
+using MainApp.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 
 namespace MainApp.Controllers
 {
     public class JobOfferController : Controller
     {
-        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        private readonly DataContext _context;
+
+        public JobOfferController(DataContext context)
         {
-            new JobOffer{Id=1, JobTitle="Backend Developer", City="Radzyń Podlaski", Company="Spółdzielnia mleczarska SPOMLEK", Description="Opis w budowie", ExpirationDate=DateTime.Now },
-            new JobOffer{Id=2, JobTitle="Frontend Developer" },
-            new JobOffer{Id=3, JobTitle="Korektor", City="Rzabików", Company="PGA", Description="Zatródnimy korektora", ExpirationDate=DateTime.MaxValue},
-            new JobOffer{Id=4, JobTitle="Manager" },
-            new JobOffer{Id=5, JobTitle="Teacher" },
-            new JobOffer{Id=6, JobTitle="Cook" },
-            new JobOffer{Id=7, JobTitle="Volkswagen Passat B7 2013 przebieg 87000", City="Pszczyna", Company="PGA", Description="Wymieniony olej w skrzyni biegów, nowy rozrząd, nowe opony. Samochód jest w stanie bardzo dobrym i nie wymaga żadnego wkładu finansowego. Idealny dla rodzin a także idealny na górskie wypady.Samochód godny polecenia. Cena: 56.500,00 brutto", ExpirationDate=DateTime.MaxValue}
-        };
-        public IActionResult Index()
-        {
-            return View("Index", _jobOffers);
+            _context = context;
         }
-        public IActionResult Details(int id)
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery(Name = "search")] string searchString)
         {
-            if (id > _jobOffers.Count || id < 1)
-                return new NotFoundResult();
-            var model = _jobOffers[id-1];
-            return View("Details", model);
+            if (string.IsNullOrEmpty(searchString))
+                return View(await _context.JobOffers.Include(x => x.Company).ToListAsync());
+
+            List<JobOffer> searchResult = await _context
+                .JobOffers.Include(x => x.Company)
+                .Where(o => o.JobTitle.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                .ToListAsync();
+
+            return View(searchResult);
         }
-        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest($"id shouldn't not be null");
+            }
+            var offer = await _context.JobOffers.FirstOrDefaultAsync(x => x.Id == id.Value);
+            if (offer == null)
+            {
+                return NotFound($"offer not found in DB");
+            }
+
+            return View(offer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(JobOffer model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var offer = await _context.JobOffers.FirstOrDefaultAsync(x => x.Id == model.Id);
+            offer.JobTitle = model.JobTitle;
+            offer.Description = model.Description;
+            _context.Update(offer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", new { id = model.Id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest($"id should not be null");
+            }
+
+            _context.JobOffers.Remove(new JobOffer() { Id = id.Value });
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            var model = new JobOfferCreateView
+            {
+                Companies = await _context.Companies.ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(JobOfferCreateView model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Companies = await _context.Companies.ToListAsync();
+                return View(model);
+            }
+
+            JobOffer jo = new JobOffer
+            {
+                CompanyId = model.CompanyId,
+                Description = model.Description,
+                JobTitle = model.JobTitle,
+                Location = model.Location,
+                SalaryFrom = model.SalaryFrom,
+                SalaryTo = model.SalaryTo,
+                ValidUntil = model.ValidUntil,
+                Created = DateTime.Now
+            };
+
+            await _context.JobOffers.AddAsync(jo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var offer = await _context.JobOffers
+                .Include(x => x.Company)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return View(offer);
+        }
     }
 }
--- MainApp/MainApp/Models/JobOfferModel.cs
+++ MainApp/MainApp/Models/JobOfferModel.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MainApp.Models
 {
     public class JobOffer
     {
         public int Id { get; set; }
+        [Display(Name = "Job title")]
+        [Required]
         public string JobTitle { get; set; }
-        public string Company { get; set; }
-        public DateTime ExpirationDate { get; set; }
-        public string City { get; set; }
+        public virtual Company Company { get; set; }
+        public virtual int CompanyId { get; set; }
+        [Display(Name = "Salary from")]
+        public decimal? SalaryFrom { get; set; }
+        [Display(Name = "Salary to")]
+        public decimal? SalaryTo { get; set; }
+        public DateTime Created { get; set; }
+        public string Location { get; set; }
+        [Required]
+        [MinLength(5)]
         public string Description { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
+        [Display(Name = "Valid until")]
+        public DateTime? ValidUntil { get; set; }
         public List<Application> JobApplications { get; set; } = new List<Application>();
+
     }
 }
