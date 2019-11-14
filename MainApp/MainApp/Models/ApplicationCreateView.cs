961b086c901dea87d8e27fb6dc5fa72b969404d6
Jakub Drak Sbahi <01130568@pw.edu.pl>
2019-11-14 21:25:04 +0100

    Add application creating and create button.

---
+++ MainApp/MainApp/Models/ApplicationCreateView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainApp.Models
+{
+    public class ApplicationCreateView : Application
+    {
+        public IEnumerable<JobOffer> JobOffers { get; set; }
+    }
+}
--- MainApp/MainApp/Models/ApplicationModel.cs
+++ MainApp/MainApp/Models/ApplicationModel.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MainApp.Models
 {
     public class Application
     {
         public int Id { get; set; }
+        [Display(Name = "Offer")]
         public int OfferId { get; set; }
+        [Required]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Email address")]
         public string EmailAddress { get; set; }
+        [Required]
+        [Display(Name = "Contact agreement")]
         public bool ContactAgreement { get; set; }
+        [Required]
         public string CvUrl { get; set; }
     }
 }
