9763eb52fd122946ea50380f7c66a404fdbc3fe2
JakubBilski <jakub.michal.bilski@gmail.com>
2019-12-01 15:29:05 +0100

    Changed fontawesome usage, so now it works. Added HR views similar to existing JobOffer views.
    Related Work Items: #39, #86, #89, #94, #177, #178, #179, #180

---
+++ MainApp/MainApp/Migrations/20191130175722_Add-Migration ChangeHR.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MainApp.Migrations
+{
+    public partial class AddMigrationChangeHR : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HRs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    CompanyId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HRs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HRs_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HRs_CompanyId",
+                table: "HRs",
+                column: "CompanyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HRs");
+        }
+    }
+}
