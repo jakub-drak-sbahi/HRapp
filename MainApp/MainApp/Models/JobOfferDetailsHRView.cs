e36d74e197346a7f41fc38899d5b1288c7e92028
JakubBilski <jakub.michal.bilski@gmail.com>
2019-12-26 00:51:20 +0100

    Add JobOffer views distinction based on role. Change JobOffer controller, left empty places for authorization. Add needed models for these views.
    Related Work Items: #36, #42, #196

---
+++ MainApp/MainApp/Models/JobOfferDetailsHRView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MainApp.Models
+{
+	public class JobOfferDetailsHRView : JobOffer 
+	{
+		public JobOffer Offer { get; set; }
+		public HR HR { get; set; }
+        public IEnumerable<Application> Applications { get; set; }
+    }
+}
