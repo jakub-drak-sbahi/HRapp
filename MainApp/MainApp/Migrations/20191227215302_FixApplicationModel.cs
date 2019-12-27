78d52337933dfece12d1c511d8d0fafba945d962
jakub-drak-sbahi <jakubdrak98@gmail.com>
2019-12-27 22:59:53 +0100

    Fix Application model

---
+++ MainApp/MainApp/Migrations/20191227215302_FixApplicationModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MainApp.Migrations
+{
+    public partial class FixApplicationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HRId",
+                table: "JobApplications",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_HRId",
+                table: "JobApplications",
+                column: "HRId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobApplications_HRs_HRId",
+                table: "JobApplications",
+                column: "HRId",
+                principalTable: "HRs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobApplications_HRs_HRId",
+                table: "JobApplications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobApplications_HRId",
+                table: "JobApplications");
+
+            migrationBuilder.DropColumn(
+                name: "HRId",
+                table: "JobApplications");
+        }
+    }
+}
