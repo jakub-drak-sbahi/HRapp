2e67376cf746658752cfa6f70abb54dec1e98d34
JakubBilski <jakub.michal.bilski@gmail.com>
2020-01-05 16:18:02 +0100

    Add comment views, controller methods in application controller and add to seed
    Related Work Items: #254, #255, #256, #257, #258, #259, #260, #261

--- MainApp/MainApp/Migrations/20200104175317_Add comments to data context and seed.Designer.cs
+++ MainApp/MainApp/Migrations/20200104175317_Add comments to data context and seed.Designer.cs
@@ -4,14 +4,16 @@ using MainApp.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MainApp.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200104175317_Add comments to data context and seed")]
+    partial class Addcommentstodatacontextandseed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
